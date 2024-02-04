62087387a26ee6aa5485634af2a49d5e89c1318a
inomu <main.it07@gmail.com>
2024-02-04 19:27:29 +0900

    add:便利コード追加

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void){    // 自分の得意な言語で
     // Let's チャレンジ！！
@@ -9,6 +10,13 @@ int main(void){    // 自分の得意な言語で
     if( 30 == atoi(str) ){
         printf("200\n");
     }
+    /* 便利コード
+     * １行の中に複数の取り出したい要素があった場合に使う
+    char *sp = strtok( str , " ");
+    const int num_of_applicant = atoi(sp);
+    sp = strtok( NULL , " ");
+    const int expect_x = atoi(sp);
+    便利コード */
 
     if( 50 == atoi(str) ){
         printf("400\n");
