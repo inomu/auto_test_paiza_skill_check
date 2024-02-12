807690b80c1d97efcabecf209461b2703c11258e
inomu <main.it07@gmail.com>
2024-02-12 23:42:56 +0900

    add:便利コード追加

--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEVELOP
+
+#ifdef DEVELOP/*{{{*/
+    void print_array( int a_H , int a_W  , char au_data[a_H][a_W] ){
+        
+        for( int i = 0 ; i < a_H ; i++){
+            for( int j = 0 ; j < a_W ; j++ ){
+                printf("%c" , au_data[i][j]);
+            }
+            printf("\n");
+        }
+        
+        printf("\n");
+    }
+#else
+    void print_array( int a_H , int a_W  , unsigned char au_data[a_H][a_W] ){}
+#endif
+/*}}}*/
+
 int main(void){    // 自分の得意な言語で
     // Let's チャレンジ！！
     char str[1000];
